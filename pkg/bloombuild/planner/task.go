2288b2d4c7bfa66f073a8d41c6a224d692a9cd9f
Salva Corts <salva.corts@grafana.com>
2024-05-31 11:37:15 +0200

    Spin up goroutines to process deletes

--- pkg/bloombuild/planner/task.go
+++ pkg/bloombuild/planner/task.go
@@ -2,23 +2,42 @@ package planner
 
 import (
 	"context"
+	"github.com/grafana/loki/v3/pkg/storage/stores/shipper/bloomshipper"
 	"time"
 
 	"github.com/grafana/loki/v3/pkg/bloombuild/protos"
 )
 
-type Task struct {
+type QueueTask struct {
 	*protos.Task
+
+	resultsChannel chan *TaskResult
 
 	// Tracking
 	queueTime time.Time
 	ctx       context.Context
 }
 
-func NewTask(ctx context.Context, queueTime time.Time, task *protos.Task) *Task {
-	return &Task{
-		Task:      task,
-		ctx:       ctx,
-		queueTime: queueTime,
+func NewTask(
+	ctx context.Context,
+	queueTime time.Time,
+	task *protos.Task,
+	resultsChannel chan *TaskResult,
+) *QueueTask {
+	return &QueueTask{
+		Task:           task,
+		resultsChannel: resultsChannel,
+		ctx:            ctx,
+		queueTime:      queueTime,
+	}
+}
+
+type TaskResult struct {
+	metas []bloomshipper.MetaRef
+}
+
+func NewTaskResult(metas []bloomshipper.MetaRef) *TaskResult {
+	return &TaskResult{
+		metas: metas,
 	}
 }
